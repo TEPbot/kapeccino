b589da75c898e3fd2cebe33dff593a4f2f623568
TEPbot <tepbot2@gmail.com>
2024-09-15 18:05:29 +0800

    Update uptime.js

--- scripts/cmds/uptime.js
+++ scripts/cmds/uptime.js
@@ -5,7 +5,7 @@ const exec = util.promisify(require('child_process').exec);
 module.exports = {
   config: {
     name: "uptime",
-    aliases: ["upt","stat"],
+    aliases: ["upt", "stat"],
     version: "1.0",
     author: "JARiF@Cock",
     role: 0,
@@ -17,7 +17,7 @@ module.exports = {
   onStart: async function ({ message }) {
 
     const uptime = process.uptime();
-    const formattedUptime = formatMilliseconds(uptime * 1000);
+    const formattedUptime = formatUptime(uptime);
 
     const totalMemory = os.totalmem();
     const freeMemory = os.freemem();
@@ -69,16 +69,9 @@ function formatUptime(seconds) {
   const days = Math.floor(seconds / 86400);
   const hours = Math.floor((seconds % 86400) / 3600);
   const minutes = Math.floor((seconds % 3600) / 60);
+  const secs = Math.floor(seconds % 60);
 
-  return `${days}d ${hours}h ${minutes}m`;
-}
-
-function formatMilliseconds(ms) {
-  const seconds = Math.floor(ms / 1000);
-  const minutes = Math.floor(seconds / 60);
-  const hours = Math.floor(minutes / 60);
-
-  return `${hours}h ${minutes % 60}m ${seconds % 60}s`;
+  return `${days}d ${hours}h ${minutes}m ${secs}s`;
 }
 
 function prettyBytes(bytes) {
